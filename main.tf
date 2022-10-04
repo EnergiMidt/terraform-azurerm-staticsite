b907a280d7004c4d9d0dadad45f7e1139ec2627f
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-10-04 21:14:54 +0200

    feature: staticsite module

--- main.tf
+++ main.tf
@@ -10,12 +10,8 @@ resource "azurerm_static_site" "static_site" {
   sku_tier = var.sku_tier
   sku_size = var.sku_size
 
-  dynamic "identity" {
-    for_each = var.identity
-    content {
-      type         = identity.value["type"]         # (Required) Specifies the type of Managed Service Identity that should be configured on this resource. Possible values are `SystemAssigned`, `UserAssigned` and `SystemAssigned, UserAssigned` (to enable both).
-      identity_ids = identity.value["identity_ids"] # (Optional) A list of Managed Identity IDs which should be assigned to this Static Site resource.
-    }
+  identity {
+    type = "SystemAssigned" # (Required) Specifies the type of Managed Service Identity that should be configured on this resource. Possible values are `SystemAssigned`, `UserAssigned` and `SystemAssigned, UserAssigned` (to enable both).
   }
 
   tags = var.tags
