ec16f46680995f6508e01d63d7869f2e96f67c47
Peder Lång Skeidsvoll <pls@computas.com>
2022-11-17 09:06:46 +0100

    Added support for app settings in static site

--- main.tf
+++ main.tf
@@ -22,3 +22,43 @@ resource "azurerm_static_site" "static_site" {
 
   tags = var.tags
 }
+
+# App settings for static app is not supported. 
+# https://github.com/hashicorp/terraform-provider-azurerm/issues/13451
+resource "azurerm_resource_group_template_deployment" "static_site_appsettings" {
+  deployment_mode     = "Incremental"
+  name                = "static-site-appsettings"
+  resource_group_name = var.resource_group.name
+
+  template_content = jsonencode(local.appSettingsTemplate)
+  parameters_content = jsonencode({
+    for i, v in var.app_settings :
+    "${i}" => {
+      value = v
+    }
+  })
+}
+
+# Generate JSON file for ARM-template
+locals {
+  appSettingsTemplate = {
+    "$schema"      = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"
+    contentVersion = "1.0.0.0"
+    parameters = ({
+      for i, v in var.app_settings :
+      "${i}" => {
+        type = "string"
+      }
+    }),
+    variables = {}
+    resources = [
+      {
+        type       = "Microsoft.Web/staticSites/config"
+        apiVersion = "2020-10-01"
+        name       = "${azurerm_static_site.static_site.name}/appsettings"
+        kind       = "string"
+        properties = var.app_settings
+      }
+    ]
+  }
+}
